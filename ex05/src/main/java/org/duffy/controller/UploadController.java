a20f53bee1104fce0efd53ee708495d7cb09009b
zzunho9 <jamlove4763@naver.com>
2021-05-02 17:30:52 +0900

    49th commit, add file uploads by a post method

--- ex05/src/main/java/org/duffy/controller/UploadController.java
+++ ex05/src/main/java/org/duffy/controller/UploadController.java
@@ -1,8 +1,12 @@
 package org.duffy.controller;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
 
@@ -14,6 +18,30 @@ public class UploadController {
 	public void uploadForm() {
 		
 		log.info("upload form");
+	}
+	
+	@PostMapping("/uploadFormAction")
+	public void uploadActionFormPost(MultipartFile[] uploadFiles, Model model) {
+		
+		String uploadFolder = "/Users/zzun_ho9/upload";
+
+		
+		for(MultipartFile file : uploadFiles) {
+			
+			log.info("--------------------------------");
+			log.info("Upload file name :"+file.getOriginalFilename());
+			log.info("Upload file size :"+file.getSize());
+			
+			File saveFile = new File(uploadFolder, file.getOriginalFilename());
+			
+			try {
+				file.transferTo(saveFile);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}//end catch
+			
+		}//end for
+		
 	}
 	
 }
