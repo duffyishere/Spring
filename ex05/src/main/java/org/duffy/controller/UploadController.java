82776980995fa64f4489df72ed79149598c29fa8
zzunho9 <jamlove4763@naver.com>
2021-05-02 17:08:03 +0900

    48th commit, Initial Setting

---
+++ ex05/src/main/java/org/duffy/controller/UploadController.java
@@ -0,0 +1,19 @@
+package org.duffy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class UploadController {
+	
+	@GetMapping("/uploadForm")
+	public void uploadForm() {
+		
+		log.info("upload form");
+	}
+	
+}
